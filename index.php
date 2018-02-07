ff700b4740f960447d3f74eaf5452372d2fd03e4
Szkolenie <Szkolenie@Blueform>
2018-02-07 09:44:05 +0100

    zmiany w pliku

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Bartek Git</title>
     </head>
     <body>
         <?php
-        // put your code here
+        echo '<h1>Siemaneczko Elo Witam </h1>';
         ?>
+        
+        <p>Ale poszły zmiany :D</p>
     </body>
 </html>
